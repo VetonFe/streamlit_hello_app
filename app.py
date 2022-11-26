321fd54d4bc668aeefc340374e397268cd18e816
VetonFe <vetonferizaj4@gmail.com>
2022-11-26 14:29:43 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 import streamlit as st
-from datetime import date
+from datetime import date, datetime
 
 st.title("Hello Streamlit App")
 
 st.header("My name is Veton and this is my first deployed app")
 
 today = date.today()
+time_now = datetime.now()
+st.write(time_now)
 st.write(today)
